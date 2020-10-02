ea8c006921a4955682dda8e5b5fa17bd1ac74552
Josh <joshmoo2009@hotmail.com>
2020-10-02 13:56:06 +0100

    Followed a couple more lessons

---
+++ Assets/Scripts/SpawnManager.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnManager : MonoBehaviour
+{
+    [SerializeField] private GameObject _spawnEntity;
+    [SerializeField] private float _secondsBetweenSpawning = 5.0f;
+
+    void Start()
+    {
+        Spawn();
+        StartCoroutine(SpawnRoutine());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    IEnumerator SpawnRoutine()
+    {
+        while(true)
+        {
+            yield return new WaitForSeconds(_secondsBetweenSpawning);
+            Spawn();
+        }
+    }
+
+    void Spawn()
+    {
+        Debug.Log("Spawned");
+        Vector3 spawnPosition = new Vector3(Random.Range(-8f, 8f), 7, 0);
+        Instantiate(_spawnEntity, spawnPosition, Quaternion.identity);
+    }
+}
